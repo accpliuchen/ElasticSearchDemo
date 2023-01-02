459e9166192f3c66a86344bdd3856932b4452a55
cl6016 <cl6016@nyu.edu>
2023-01-01 22:41:40 -0500

    changed code

--- src/main/java/com/test/Test.java
+++ src/main/java/com/test/Test.java
@@ -6,6 +6,7 @@ import org.elasticsearch.action.admin.indices.create.CreateIndexRequest;
 import org.elasticsearch.action.admin.indices.create.CreateIndexResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.update.UpdateRequest;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.settings.Settings;
@@ -13,11 +14,11 @@ import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.index.mapper.ObjectMapper;
 
 import java.io.IOException;
+import java.util.Date;
 
+//https://cloud.tencent.com/developer/article/1459354
 public class Test
 {
-
-
         public static void main(String args[]){
           try{
               //Low Level Client init
@@ -29,23 +30,32 @@ public class Test
 
               // https://mkyong.com/elasticsearch/elasticsearch-hello-world-example/
               // browser http://127.0.0.1:9200/posts/doc/1
-              IndexRequest request = new IndexRequest(
-                      "posts", //index name
-                      "doc",  // type
-                      "3");   // doc id
-              String jsonString = "{" +
-                      "\"user\":\"kimchy\"," +
-                      "\"postDate\":\"2013-01-30\"," +
-                      "\"message\":\"trying out Elasticsearch\"" +
-                      "}";
-              request.source(jsonString, XContentType.JSON);
+              // search  http://127.0.0.1:9200/posts/doc/_search?q=_id:1
+//              IndexRequest request = new IndexRequest(
+//                      "posts", //index name
+//                      "doc",  // type
+//                      "3");   // doc id
+//              String jsonString = "{" +
+//                      "\"user\":\"kimchy\"," +
+//                      "\"postDate\":\"2013-01-30\"," +
+//                      "\"message\":\"trying out Elasticsearch\"" +
+//                      "}";
+//              request.source(jsonString, XContentType.JSON);
 
-              IndexResponse indexResponse = client.index(request);
 
-              String index = indexResponse.getIndex();  //index名称，类型等信息
-              String type = indexResponse.getType();
-              String id = indexResponse.getId();
-              long version = indexResponse.getVersion();
+//              IndexResponse indexResponse = client.index(request);
+//
+//              String index = indexResponse.getIndex();  //index名称，类型等信息
+//              String type = indexResponse.getType();
+//              String id = indexResponse.getId();
+//              long version = indexResponse.getVersion();
+
+              UpdateRequest request = new UpdateRequest("posts", "doc", "1")
+                      .doc("updated", new Date(),
+                              "message", "daily updatesss");
+              client.update(request);
+
+//
 
               lowLevelRestClient.close();
           }catch(Exception e){
